8d5489f759e1d47111f742eb44dd8e39a397fbcb
Steven Scott <steven@codemettle.com>
2021-09-21 14:06:40 -0400

    refactor for more tests

--- src/test/scala/autong/TestAutoStorage.scala
+++ src/test/scala/autong/TestAutoStorage.scala
@@ -1,60 +1,30 @@
 package autong
 
 import autong.AutoNGMain.doWork
-import autong.UIInterface.{Button, Card, Page, SideNavButton, SideNavEntry}
-import zio.{IO, Ref, Task, ZIO}
+import autong.TestUIInterface.{createPage, TestButton, TestSideNavButton, TestSideNavEntry}
 import zio.test.Assertion._
 import zio.test._
 
-import scala.scalajs.js
-
 object TestAutoStorage extends DefaultRunnableSpec {
-
-  case class TestButton(clickCount: Ref[Int], name: Option[String] = None, _disabled: Boolean = false) extends Button {
-    val click: Task[Unit]       = clickCount.update(_ + 1)
-    val disabled: Task[Boolean] = Task(_disabled)
-  }
-
-  case class TestSideNavButton(
-      _name: String,
-      _amountStored: js.UndefOr[String] = js.undefined,
-      _totalStorage: js.UndefOr[String] = js.undefined,
-      click: Task[Unit] = Task.unit,
-  ) extends SideNavButton {
-    val name: IO[Option[Throwable], String]         = ZIO.succeed(_name).asSomeError
-    val amountStored: IO[Option[Throwable], String] = ZIO.fromOption(_amountStored.toOption)
-    val totalStorage: IO[Option[Throwable], String] = ZIO.fromOption(_totalStorage.toOption)
-  }
-
-  case class TestSideNavEntry(navBtn: TestSideNavButton, upgBtn: js.UndefOr[TestButton] = js.undefined)
-      extends SideNavEntry {
-    val navButton: IO[Option[Throwable], SideNavButton] = ZIO.succeed(navBtn).asSomeError
-    val upgradeButton: IO[Option[Throwable], Button]    = ZIO.fromOption(upgBtn.toOption)
-  }
-
-  private def createPage(name: String, _cards: Vector[Card] = Vector.empty) = new Page {
-    val pageName: IO[Option[Throwable], String]    = ZIO.succeed(name).asSomeError
-    val cards: IO[Option[Throwable], Vector[Card]] = ZIO.succeed(_cards).asSomeError
-  }
 
   private def runWithOpts(opts: Options) =
     for {
-      oilClickRef   <- Ref.make(0)
-      metalClickRef <- Ref.make(0)
-      gemClickRef   <- Ref.make(0)
-      woodClickRef  <- Ref.make(0)
+      oilUpgrade   <- TestButton.make()
+      metalUpgrade <- TestButton.make(disabled = true)
+      gemUpgrade   <- TestButton.make()
+      woodUpgrade  <- TestButton.make(disabled = true)
       rescsEntry = TestSideNavEntry(TestSideNavButton("Earth Resources"))
-      oilEntry   = TestSideNavEntry(TestSideNavButton("Oil", "1M", "1M"), TestButton(oilClickRef))
-      metalEntry = TestSideNavEntry(TestSideNavButton("Metal", "1M", "1M"), TestButton(metalClickRef, _disabled = true))
-      gemEntry   = TestSideNavEntry(TestSideNavButton("Gem", "5M", "6M"), TestButton(gemClickRef))
-      woodEntry  = TestSideNavEntry(TestSideNavButton("Wood", "5M", "6M"), TestButton(woodClickRef, _disabled = true))
+      oilEntry   = TestSideNavEntry(TestSideNavButton("Oil", "1M", "1M"), oilUpgrade)
+      metalEntry = TestSideNavEntry(TestSideNavButton("Metal", "1M", "1M"), metalUpgrade)
+      gemEntry   = TestSideNavEntry(TestSideNavButton("Gem", "5M", "6M"), gemUpgrade)
+      woodEntry  = TestSideNavEntry(TestSideNavButton("Wood", "5M", "6M"), woodUpgrade)
       sideNavs   = Vector(rescsEntry, oilEntry, metalEntry, gemEntry, woodEntry)
       layers     = TestStorage.default ++ TestUIInterface.create(createPage("Lava"), sideNavs)
       _               <- doWork(Options.setDefaults(opts), 0).provideCustomLayer(layers)
-      oilClickCount   <- oilClickRef.get
-      metalClickCount <- metalClickRef.get
-      gemClickCount   <- gemClickRef.get
-      woodClickCount  <- woodClickRef.get
+      oilClickCount   <- oilUpgrade.clicks
+      metalClickCount <- metalUpgrade.clicks
+      gemClickCount   <- gemUpgrade.clicks
+      woodClickCount  <- woodUpgrade.clicks
     } yield (oilClickCount, metalClickCount, gemClickCount, woodClickCount)
 
   final val spec = suite("Auto Storage")(
--- src/test/scala/autong/TestUIInterface.scala
+++ src/test/scala/autong/TestUIInterface.scala
@@ -1,7 +1,9 @@
 package autong
 
-import autong.UIInterface.{Page, SideNavEntry}
-import zio.{Has, Task, ULayer, ZIO, ZLayer}
+import autong.UIInterface._
+import zio._
+
+import scala.scalajs.js
 
 object TestUIInterface {
 
@@ -11,6 +13,43 @@ object TestUIInterface {
 
       override def sideNavEntries: Task[Vector[SideNavEntry]] = ZIO.succeed(sideNavs)
     }
+  }
+
+  case class TestButton(clickCount: Ref[Int], name: Option[String] = None, _disabled: Boolean = false) extends Button {
+    val click: Task[Unit]       = clickCount.update(_ + 1)
+    val disabled: Task[Boolean] = Task(_disabled)
+
+    val clicks: UIO[Int] = clickCount.get
+  }
+
+  object TestButton {
+
+    def make(name: js.UndefOr[String] = js.undefined, disabled: Boolean = false): UIO[TestButton] = for {
+      r <- Ref.make(0)
+    } yield TestButton(r, name.toOption, disabled)
+
+  }
+
+  case class TestSideNavButton(
+      _name: String,
+      _amountStored: js.UndefOr[String] = js.undefined,
+      _totalStorage: js.UndefOr[String] = js.undefined,
+      click: Task[Unit] = Task.unit,
+  ) extends SideNavButton {
+    val name: IO[Option[Throwable], String]         = ZIO.succeed(_name).asSomeError
+    val amountStored: IO[Option[Throwable], String] = ZIO.fromOption(_amountStored.toOption)
+    val totalStorage: IO[Option[Throwable], String] = ZIO.fromOption(_totalStorage.toOption)
+  }
+
+  case class TestSideNavEntry(navBtn: TestSideNavButton, upgBtn: js.UndefOr[TestButton] = js.undefined)
+      extends SideNavEntry {
+    val navButton: IO[Option[Throwable], SideNavButton] = ZIO.succeed(navBtn).asSomeError
+    val upgradeButton: IO[Option[Throwable], Button]    = ZIO.fromOption(upgBtn.toOption)
+  }
+
+  def createPage(name: String, _cards: Vector[Card] = Vector.empty): Page = new Page {
+    val pageName: IO[Option[Throwable], String]    = ZIO.succeed(name).asSomeError
+    val cards: IO[Option[Throwable], Vector[Card]] = ZIO.succeed(_cards).asSomeError
   }
 
 }
